1e94d0ca05349ac02676c73e338615f17bcadfb6
Leander Thiele <lthiele@princeton.edu>
2021-02-09 01:24:36 -0500

    compiling version

--- compile.sh
+++ compile.sh
@@ -2,12 +2,14 @@
 
 export LIBTORCH=${HOME}/libtorch
 
-g++ -std=c++17 -Wall -Wextra -Wno-unused-parameter \
+g++ -std=c++17 -O3 -Wall -Wextra -Wno-unused-parameter \
     -D_GLIBCXX_USE_CXX11_ABI=0 \
     -DTESTS \
     -DMULTI_WORKERS \
+    -DMULTI_ROOT \
     -I${LIBTORCH}/include/torch/csrc/api/include/ \
     -I${LIBTORCH}/include \
     voxelize_gpu.cpp \
     -L${LIBTORCH}/lib \
-    -ltorch -ltorch_cpu -lc10 -fopenmp
+    -ltorch -ltorch_cpu -lc10 -lc10_cuda -lcudart \
+    -fopenmp
