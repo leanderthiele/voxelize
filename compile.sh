675e754a443883119f3cd796ed02424e8656851c
leander <leander97@gmx.de>
2021-02-14 14:47:38 -0500

    linkage to hdf5 in header

--- compile.sh
+++ compile.sh
@@ -9,7 +9,8 @@ g++ -std=c++17 -O0 -g3 -Wall -Wextra -Wno-unused-parameter \
     -DMULTI_ROOT \
     -I${LIBTORCH}/include/torch/csrc/api/include/ \
     -I${LIBTORCH}/include \
-    voxelize_gpu.cpp \
+    -o $1 $1.cpp \
     -L${LIBTORCH}/lib \
     -ltorch -ltorch_cpu -lc10 -lc10_cuda -lcudart \
+    -lhdf5 -lhdf5_cpp \
     -fopenmp
