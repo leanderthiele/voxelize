a66744da912b08a5c66f5886575116324aa0f813
leander <leander97@gmx.de>
2021-02-15 20:28:54 -0500

    fixed vol_norm bug

--- queues_implementation.hpp
+++ queues_implementation.hpp
@@ -100,7 +100,7 @@ cpu_queue_item::cpu_queue_item (std::shared_ptr<gpu_batch_queue_item> gpu_result
         for (size_t ii=0; ii != x->box_indices.size(); ++ii)
             #pragma loop_count (1, 2, 3)
             for (int64_t dd = 0; dd != globals.dim; ++dd)
-                weights.push_back(x->weights[ii*globals.dim+dd] * gpu_result->vol_norm[ii]);
+                weights.push_back(x->weights[ii*globals.dim+dd] * x->vol_norm[ii]);
     }
     #endif // WORKERS_MAKE_BATCHES
 
