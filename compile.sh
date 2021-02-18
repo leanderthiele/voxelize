eb0b4454c84112baf3e9cbfb1f74521d4c30b434
leander <leander97@gmx.de>
2021-02-18 14:37:20 -0500

    added cuda to include paths

--- compile.sh
+++ compile.sh
@@ -2,12 +2,14 @@
 
 export TORCH=${HOME}/pytorch-install
 export CUDNN=/usr/local/cudnn/cuda-11.0/8.0.2
+export CUDA=/usr/local/cuda-11.0
 
 g++ -std=c++17 -O3 -g3 \
     -Wall -Wextra -Wunused-result -Wno-unused-parameter \
     -D_GLIBCXX_USE_CXX11_ABI=1 \
     -I${TORCH}/include/torch/csrc/api/include/ \
     -I${TORCH}/include \
+    -I${CUDA}/include \
     -I${CUDNN}/include \
     -o $1 $1.cpp \
     -L${TORCH}/lib \
